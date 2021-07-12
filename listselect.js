9bf8b50a9e98819eff4521ef3f0f0b6b5fc50b63
xmeatballx <erik.jensen5@pcc.edu>
2021-07-12 00:47:20 -0700

    experimenting with mobile accelerometer control

--- listselect.js
+++ listselect.js
@@ -3,9 +3,10 @@ let interactiveArt = document.getElementById('interactive_art_id');
 let artPieces = document.getElementsByClassName('interactive_art');
 let webDesign = document.getElementById('web_design_id');
 let websites = document.getElementsByClassName('web_design');
+let back = document.getElementsByClassName('back');
 
 interactiveArt.onclick = () => {
-	console.log('clicked');
+	// DeviceOrientationEvent.requestPermission();
 	mainList.forEach(element => {
 		element.style.display = "none";
 	})
@@ -16,7 +17,6 @@ interactiveArt.onclick = () => {
 }
 
 webDesign.onclick = () => {
-	console.log('clicked');
 	mainList.forEach(element => {
 		element.style.display = "none";
 	})
@@ -24,4 +24,20 @@ webDesign.onclick = () => {
 	websites.forEach(piece => {
 		piece.style.display = "block";
 	})
+}
+
+for (var i = 0; i < back.length; i++) {
+	back[i].onclick = () => {
+		artPieces.forEach(piece => {
+			piece.style.display = "none";
+		})
+
+		websites.forEach(piece => {
+			piece.style.display = "none";
+		})
+
+		mainList.forEach(element => {
+			element.style.display = "block";
+		})
+	}
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,29 @@
 let rotValue = 0;
+let rotValArray = [];
+var inc=0;
+let drippy = [];
+let bounds = true;
+let numDrips=5;
+let bg, drip;
+
 function setup() {
 	let c = createCanvas(windowWidth, windowHeight, WEBGL);
 	c.parent('p5container');
+	// ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 5000);
 }
 
 function draw() {
-	rotValue-=.001;
+	push();
+	rotValue-=.0001;
 	rotateX(-mouseY/200 + rotValue);
 	rotateY(-mouseX/400 + rotValue);
-
-	background(0);
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    rotateX(radians(rotationX + rotValue))
+    rotateY(radians(rotationY+rotValue))
+}
 	fill(0);
-	stroke(0,255,0);
-	strokeWeight(.5);
+	stroke(`rgba(0,255,0,.5)`);
+	strokeWeight(1);
 	sphere(windowWidth);
+	pop();
 }
