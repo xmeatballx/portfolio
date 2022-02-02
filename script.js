6872dcdff645160b6eb227633e504d1af1dd8dd8
Erik Jensen <erik@mac-mini.myfiosgateway.com>
2022-02-01 22:43:25 -0500

    added fullpage scrolling library and started work section

--- script.js
+++ script.js
@@ -58,11 +58,5 @@ window.addEventListener("load", () => {
   // });
 });
 
-new fullpage('#fullpage', {
-	//options here
-	autoScrolling:true,
-	scrollHorizontally: true
-});
 
-//methods
-fullpage_api.setAllowScrolling(false);
+new Pageable(".container")
