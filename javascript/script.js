93c518aee37cf243f5811304a6000710c1abb168
xmeatballx <erik.jensen5@pcc.edu>
2022-02-14 23:20:22 -0500

    organized file structure

--- javascript/script.js
+++ javascript/script.js
@@ -7,29 +7,11 @@ window.addEventListener("load", () => {
   });
   // get our plane element
   const planeElement = document.getElementsByClassName("plane")[0];
-  const planeElement2 = document.getElementsByClassName("plane2")[0];
 
   // set our initial parameters (basic uniforms)
   const params = {
-    vertexShaderID: "plane-vs", // our vertex shader ID
-    fragmentShaderID: "plane-fs", // our fragment shader ID
-    uniforms: {
-      time: {
-        name: "uTime", // uniform name that will be passed to our shaders
-        type: "1f", // this means our uniform is a float
-        value: 0,
-      },
-      resolution: {
-        name: "resolution",
-        type: "2f",
-        value: new Vec2(window.innerWidth, window.innerHeight),
-      },
-    },
-  };
-
-  const params2 = {
-    vertexShaderID: "plane2-vs", // our vertex shader ID
-    fragmentShaderID: "plane2-fs", // our fragment shader ID
+    vertexShader: vertexShader, // our vertex shader ID
+    fragmentShader: fragmentShader, // our fragment shader ID
     uniforms: {
       time: {
         name: "uTime", // uniform name that will be passed to our shaders
@@ -45,17 +27,8 @@ window.addEventListener("load", () => {
   };
   // create our plane using our curtains object, the HTML element and the parameters
   const plane = new Plane(curtains, planeElement, params);
-  // const plane2 = new Plane(curtains, planeElement2, params2);
-
   plane.onRender(() => {
     // use the onRender method of our plane fired at each requestAnimationFrame call
     plane.uniforms.time.value += 0.001; // update our time uniform value
   });
-
-  // plane2.onRender(() => {
-  //   // use the onRender method of our plane fired at each requestAnimationFrame call
-  //   plane.uniforms.time.value += 0.001; // update our time uniform value
-  // });
 });
-
-// new Pageable(".container", { animation: 1000 });
